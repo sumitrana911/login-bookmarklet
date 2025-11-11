39f760384278ab64505405977c9949fae260deab
Sumit rana <40038329+sumitrana911@users.noreply.github.com>
2025-11-11 23:52:18 +0530

    Create hardcoded.js

---
+++ bookmarklets/hardcoded.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+  document.getElementById('username').value='yourUsername';
+  document.getElementById('password').value='yourPassword';
+  document.getElementById('submit').click();
+})();
